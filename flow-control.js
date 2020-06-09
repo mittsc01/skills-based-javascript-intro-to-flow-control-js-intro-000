eead688953fc08caad36307f4ca4481404d8defb
Scott Mittman <sm2884@nau.edu>
2020-06-09 22:15:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,11 +5,17 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
+  if (age>=13 && age<=19){
+    return "You are a teenager!";
+  }
+  else {
+    return "You are not a teenager";
+  }
 
 }
 
 function ageChecker(age) {
-
+    
 }
 
 function ternaryTeenager(age) {
