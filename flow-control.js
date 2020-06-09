fb19b87a45da53f808ff68b3f9f288ba76fa1cf2
Scott Mittman <sm2884@nau.edu>
2020-06-09 22:29:12 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -15,7 +15,15 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if (age<=12){
+    return "You are a kid"
+  }
+  else if (age>12 && age<20){
+    return "You are a teenager!"
+  }
+  else {
+    return "You are a grownup"
+  }
 }
 
 function ternaryTeenager(age) {
