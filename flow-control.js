850a1f75434a128b79b9d322d18752880b52ae78
Scott Mittman <sm2884@nau.edu>
2020-06-09 22:34:30 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,5 +31,12 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
+  switch(age){
+    case (age>12 && age<20):
+      return "You are a teenager";
+    
+    default ():
+      return "You have an age"
+  }
 
 }
